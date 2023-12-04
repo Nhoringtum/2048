12bb11348ec330f1f73d4c5281fe802c0788b742
Nodeions <147034141+Nhoringtum@users.noreply.github.com>
2023-12-04 16:03:15 +0100

    Update 2048.3.py
    protect float, continuer le code qu'on a fait avec le prof

--- 2048.3.py
+++ 2048.3.py
@@ -2,11 +2,10 @@ import os
 import time
 import random
 import tools
-import numpy
 
 
 def GetGrid(length: int) -> list[list[str]]:
-    grille: list[list[str]] = []
+    grid: list[list[str]] = []
     i: int = 0
     while i < length:
         line: list[str] = []
@@ -15,13 +14,30 @@ def GetGrid(length: int) -> list[list[str]]:
             line.append("")
             j += 1
 
-        grille.append(line)
+        grid.append(line)
         i += 1
 
-    return grille
+    return grid
 
-def SpawnTiles(grid, length):
-    dropRate: list[float] = [0.9, 0.1]
+# available: list[tuple[int, int]]
+
+# [
+#    (0,0),
+#    (0,1),
+#    (0,2),
+#    (3,3),
+#]
+
+# available_index: int = random.randint(0,3)
+
+# available_coordinate: tuple[int, int] = available[available_index]
+
+# del available[available_index]
+
+# available.remove(available_coordinate)
+
+def SpawnTiles(grid, length, x, y):
+    dropRate: list[float] = [x, y]
     tiles: list[int] = [2, 4]
     randomTiles: float = random.random()
     randomX: int = random.randint(0, length-1)
@@ -31,9 +47,16 @@ def SpawnTiles(grid, length):
     else:
         grid[randomX][randomY] = tiles[1]
 
+print("Taille de la grille : ")
 GRID_LENGTH: int = tools.ask_int()
-grid: list[list[str]] = GetGrid(GRID_LENGTH)
-SpawnTiles(grid, GRID_LENGTH)
+print("Proba pour 2")
+proba_2: int = tools.ask_int()
+print("Proba pour 4")
+proba_4: int = tools.ask_int()
+while True :
+    grid: list[list[str]] = GetGrid(GRID_LENGTH)
+
+    SpawnTiles(grid, GRID_LENGTH, proba_2, proba_4)
 
 
 i: int = 0
