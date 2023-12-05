4174aab923a5f90c5794983b083eb8eda401c176
Nodeions <147034141+Nhoringtum@users.noreply.github.com>
2023-12-05 10:53:55 +0100

    Update 2048.3.py
    add les trucs à faire

--- 2048.3.py
+++ 2048.3.py
@@ -2,8 +2,8 @@ import os
 import time
 import random
 import tools
-
-
+# Recap de truc à faire : finir le systeme avec la liste available (la créer selon la taille du tableau, supprimer les index disponibles à chaque spawn et chaque mouvement)
+# faire en sorte de décaler les tuiles dans la direction demandé du joueur mais en prenant en compte si l'index existe. Puis prog la fusion des tuiles (se passe avant le décalage)
 def GetGrid(length: int) -> list[list[str]]:
     grid: list[list[str]] = []
     i: int = 0
@@ -19,6 +19,7 @@ def GetGrid(length: int) -> list[list[str]]:
 
     return grid
 
+
 # available: list[tuple[int, int]]
 
 # [
@@ -26,7 +27,7 @@ def GetGrid(length: int) -> list[list[str]]:
 #    (0,1),
 #    (0,2),
 #    (3,3),
-#]
+# ]
 
 # available_index: int = random.randint(0,3)
 
@@ -40,29 +41,35 @@ def SpawnTiles(grid, length, x, y):
     dropRate: list[float] = [x, y]
     tiles: list[int] = [2, 4]
     randomTiles: float = random.random()
-    randomX: int = random.randint(0, length-1)
-    randomY: int = random.randint(0, length-1)
+    randomX: int = random.randint(0, length - 1)
+    randomY: int = random.randint(0, length - 1)
     if randomTiles <= dropRate[0]:
         grid[randomX][randomY] = tiles[0]
     else:
         grid[randomX][randomY] = tiles[1]
 
+
+keyAllowed: list[str] = ["z", "q", "s", "d"]
+
+
 print("Taille de la grille : ")
 GRID_LENGTH: int = tools.ask_int()
 print("Proba pour 2")
-proba_2: int = tools.ask_int()
+proba_2: float = tools.ask_proba()
 print("Proba pour 4")
-proba_4: int = tools.ask_int()
-while True :
+proba_4: float = tools.ask_proba()
+while True:
     grid: list[list[str]] = GetGrid(GRID_LENGTH)
-
     SpawnTiles(grid, GRID_LENGTH, proba_2, proba_4)
-
-
-i: int = 0
-while i < GRID_LENGTH:
-    print(grid[i])
-    i += 1
-
-time.sleep(2.0)
-os.system('cls')
+    i: int = 0
+    while i < GRID_LENGTH:
+        print(grid[i])
+        i += 1
+    print("Entrez une direction avec Z haut, Q gauche, S bas et D droite")
+    key: str = tools.ask_input(keyAllowed)
+    print(keyAllowed)
+    print(key)
+    if key == "Z":
+        print(key)
+    # time.sleep(2.0)
+    # os.system('cls')
