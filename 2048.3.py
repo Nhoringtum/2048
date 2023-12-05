f749e0d8ba947fc16e6d9eec48dd81d00c97fc75
Nodeions <147034141+Nhoringtum@users.noreply.github.com>
2023-12-05 16:25:31 +0100

    Update 2048.3.py

--- 2048.3.py
+++ 2048.3.py
@@ -2,7 +2,7 @@ import os
 import time
 import random
 import tools
-# Recap de truc à faire : finir le systeme avec la liste available (la créer selon la taille du tableau, supprimer les index disponibles à chaque spawn et chaque mouvement)
+# Recap de truc à faire : finir le systeme avec la liste available (supprimer les index disponibles à chaque mouvement)
 # faire en sorte de décaler les tuiles dans la direction demandé du joueur mais en prenant en compte si l'index existe. Puis prog la fusion des tuiles (se passe avant le décalage)
 def GetGrid(length: int) -> list[list[str]]:
     grid: list[list[str]] = []
@@ -19,57 +19,92 @@ def GetGrid(length: int) -> list[list[str]]:
 
     return grid
 
+#grid: list[list[str]] = \
+#[
+ #   ["","","",""],
+ #   ["","","",""],
+#    ["","","",""],
+#    ["","","",""],
+#]
 
-# available: list[tuple[int, int]]
 
-# [
-#    (0,0),
-#    (0,1),
-#    (0,2),
-#    (3,3),
-# ]
 
-# available_index: int = random.randint(0,3)
+def GetAvailableTiles(grid: list[list[str]]) -> list[tuple[int, int]]:
+    available_tiles: list[tuple[int, int]] = []
+    
+    i: int = 0
+    while i < len(grid):
+        j: int = 0
+        while j < len(grid[i]):
+            if(grid[i][j] == ""):
+                available_tiles.append((i, j))
+            j = j + 1
+        i = i + 1
 
-# available_coordinate: tuple[int, int] = available[available_index]
+    return available_tiles 
 
-# del available[available_index]
 
 # available.remove(available_coordinate)
 
-def SpawnTiles(grid, length, x, y):
+def SpawnTiles(grid, length, x, y) -> tuple[int, int, list[tuple[int,int]]]:
+    available_tiles: list[tuple[int, int]] = GetAvailableTiles(grid)
+
+    random_index = random.randint(0, len(available_tiles) - 1)
+
+    i, j = available_tiles[random_index]
+
     dropRate: list[float] = [x, y]
     tiles: list[int] = [2, 4]
     randomTiles: float = random.random()
-    randomX: int = random.randint(0, length - 1)
-    randomY: int = random.randint(0, length - 1)
     if randomTiles <= dropRate[0]:
-        grid[randomX][randomY] = tiles[0]
+        grid[i][j] = tiles[0]
+        return i, j, available_tiles
     else:
-        grid[randomX][randomY] = tiles[1]
+        grid[i][j] = tiles[1]
+        return i, j, available_tiles
+
+def MoveTiles(available_tiles: list[tuple[int,int]], grid: list[list[str]], input: str):
+    available_tiles
+    if input == "z":
+        if available_tiles == []:
+            print("ok")
+            print(SpawnX, SpawnY)
+            #grid[SpawnX][SpawnY] = [""]
+        print(input)
+    elif input == "q":
+        print(input)
+    elif input == "s":
+        print(input)
+    elif input == "d":
+        print(input)
 
 
 keyAllowed: list[str] = ["z", "q", "s", "d"]
 
 
-print("Taille de la grille : ")
-GRID_LENGTH: int = tools.ask_int()
-print("Proba pour 2")
-proba_2: float = tools.ask_proba()
-print("Proba pour 4")
-proba_4: float = tools.ask_proba()
-while True:
+
+def PlayGame():
+    print("Taille de la grille : ")
+    GRID_LENGTH: int = tools.ask_int()
+    print("Proba pour 2")
+    proba_2: float = tools.ask_proba()
+    print("Proba pour 4")
+    proba_4: float = tools.ask_proba()
     grid: list[list[str]] = GetGrid(GRID_LENGTH)
-    SpawnTiles(grid, GRID_LENGTH, proba_2, proba_4)
-    i: int = 0
-    while i < GRID_LENGTH:
-        print(grid[i])
-        i += 1
-    print("Entrez une direction avec Z haut, Q gauche, S bas et D droite")
-    key: str = tools.ask_input(keyAllowed)
-    print(keyAllowed)
-    print(key)
-    if key == "Z":
-        print(key)
-    # time.sleep(2.0)
-    # os.system('cls')
+
+    while True:
+        print("ahhhhhhh")
+    #   available: list[tuple[int,int]] = AvailableGrid(grid)
+        SpawnX, SpawnY, available_tiles = SpawnTiles(grid, GRID_LENGTH, proba_2, proba_4)
+        i: int = 0
+        while i < GRID_LENGTH:
+            print(grid[i])
+            i += 1
+        print("Entrez une direction avec Z haut, Q gauche, S bas et D droite")
+        key: str = tools.ask_input(keyAllowed)
+        MoveTiles(available_tiles, grid, key)
+            
+        # time.sleep(2.0)
+        # os.system('cls')
+
+PlayGame()
